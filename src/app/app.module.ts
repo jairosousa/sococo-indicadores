aae6877cd2889365b46e0cb065c22b1daacb7b22
Jairo Sousa <jaironsousa@gmail.com>
2018-04-17 19:03:58 -0300

    Ajuste no Templates

---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { AppRoutes } from './app.routing';
+import { FooterModule } from './shared/footer/footer.module';
+import { NavbarModule} from './shared/navbar/navbar.module';
+import { FixedPluginModule} from './shared/fixedplugin/fixedplugin.module';
+import { NguiMapModule} from '@ngui/map';
+
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { UserComponent } from './util-template/user/user.component';
+import { TableComponent } from './util-template/table/table.component';
+import { TypographyComponent } from './util-template/typography/typography.component';
+import { IconsComponent } from './util-template/icons/icons.component';
+import { MapsComponent } from './util-template/maps/maps.component';
+import { NotificationsComponent } from './util-template/notifications/notifications.component';
+import { UpgradeComponent } from './util-template/upgrade/upgrade.component';
+import { SidebarModule } from './util-template/sidebar/sidebar.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    UserComponent,
+    TableComponent,
+    TypographyComponent,
+    IconsComponent,
+    MapsComponent,
+    NotificationsComponent,
+    UpgradeComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(AppRoutes),
+    SidebarModule,
+    NavbarModule,
+    FooterModule,
+    FixedPluginModule,
+    NguiMapModule.forRoot({apiUrl: 'https://maps.google.com/maps/api/js?key=YOUR_KEY_HERE'})
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
